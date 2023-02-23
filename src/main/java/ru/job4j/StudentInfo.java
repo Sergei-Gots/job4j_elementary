14ea4108c92efb2b85c12a0628fc1296cad1879d
Sergei Gots <Serge.Gots@gmail.com>
2023-02-23 22:32:24 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergei Gots");
+        System.out.println("2023/02/23");
+    }
+}
