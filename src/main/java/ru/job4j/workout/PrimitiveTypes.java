aa73594212291da40a81742d4f86382f3dbaf772
Sergei Gots <Serge.Gots@gmail.com>
2023-02-26 17:56:54 +0300

    7. Расстояние между точками в системе координат [#188 #394813]

--- src/main/java/ru/job4j/workout/PrimitiveTypes.java
+++ src/main/java/ru/job4j/workout/PrimitiveTypes.java
@@ -3,16 +3,16 @@ package ru.job4j.workout;
 public class PrimitiveTypes {
     public static void main(String[] args) {
 
-        double bacteriesKoePerGram = 3;
+        double bacteriasCFUPerGram = 3;
         double spaceDistanceKm = 10_000_000_000_000L;
 
-        System.out.println("bacteriesKoePerGram = " + bacteriesKoePerGram);
+        System.out.println("bacteriasCFUPerGram = " + bacteriasCFUPerGram);
         System.out.println("spaceDistanceKm = " + spaceDistanceKm);
 
-        bacteriesKoePerGram = 1E+7;
+        bacteriasCFUPerGram = 1E+7;
         spaceDistanceKm = 9.46E12;
 
-        System.out.println("bacteriesKoePerGram = " + bacteriesKoePerGram);
+        System.out.println("bacteriasCFUPerGram = " + bacteriasCFUPerGram);
         System.out.println("spaceDistanceKm = " + spaceDistanceKm);
     }
 }
