3a4abd634287700f75f53fe42c60d89332e9193f
Sergei Gots <Serge.Gots@gmail.com>
2023-02-27 18:18:22 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+/**
+ *  4.2. Тернарное сравнение [#189]
+ */
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+}
